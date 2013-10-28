a8dce30ddd99c7616ab8667b8b4cfdd08476dcaa
Bartuz <filip@bartuzi.me>
2013-10-28 23:17:55 +0100

    changes variables names and add polish comments

--- 29.10.2013/raport5.md
+++ 29.10.2013/raport5.md
@@ -2,4 +2,8 @@
 ## a)
  - najlepsza suma: 4.
  Na podstawie wyniku najlepszej sumy w pierwszego ciągu nie da się ustalić najlepszą sume w drugim ciągu ponieważ liczby nie są takie same a również najlepsza ich suma jest inna.
- 
+
+## b)
+ - dla dane5-1.txt maksymalna suma to 48
+ - dla dane5-2.txt maksymalna suma to 42
+ - dla dane5-3.txt maksymalna suma to 48
--- 29.10.2013/zadanie5.cpp
+++ 29.10.2013/zadanie5.cpp
@@ -3,29 +3,29 @@
 #include <string>
 using namespace std;
 int main(int argc, char *argv[]) {
-	int tab[2],temp,i;
+	int temp[2],max,i;
 	ifstream file;
 	string nazwaPliku;
-	for (int j = 1; j < 4; j++) {
-		nazwaPliku="";
-		nazwaPliku += "dane5-" + to_string(j) + ".txt";
-		file.open(nazwaPliku);
-		for (int i = 0; i < 2; i++){
-			file >> tab[i];
+	for (int j = 1; j < 4; j++) { //petla ktora słuzy do ładowanie kolejnych plików.
+		nazwaPliku=""; // nazwa pliku zmienia się co nowy krok w pętli
+		nazwaPliku += "dane5-" + to_string(j) + ".txt"; // ustawiam zmienną nazwePliku na nową wartość
+		file.open(nazwaPliku); // otwieram nowy plik
+		for (int i = 0; i < 2; i++){ // ładuje 2 pierwsze wartości
+			file >> temp[i]; // zapisuje je do tablicy
 		}
-		i = 0;
-		temp = tab[0] + tab[1];
-		while ( (file >> tab[i]) ) {
-			if ( tab[0] + tab[1] > temp) {
-				temp = tab[0] + tab[1];
+		i = 0; // ustawiam pomocniczą zmienna na 0, która będzie wskazywać odpowiedni indeks w tablicy, do której będe zapisywać kolejno odczytane liczby
+		max = temp[0] + temp[1]; // zapisuje maxyymalna sume jako sume 2 pierwszych liczb
+		while ( (file >> temp[i]) ) {  // wczytuje kolejna liczbe do tablicy o indeksie i
+			if ( temp[0] + temp[1] > max) { // jesli suma nowopobranej w liczby z poprzednio pobranej liczby jest wieksza od max'a
+				max = temp[0] + temp[1];  // to max ustawiam na sume 2 ostatnio pobranych liczb
 			}
-			if (i == 0) {
+			if (i == 0) { // warunek do rotacji zmienna pomocnicza
 				i++;
 			} else {
 				i--;
 			}
 		}
-		cout << "dla " << nazwaPliku << " maksymalna suma to "  << temp << endl;
-		file.close();
+		cout << "dla " << nazwaPliku << " maksymalna suma to "  << max << endl; //wypisuje wynik dla danego pliku
+		file.close(); // i ów plik zamykam
 	}
 }

e53e34f6883ce2a37d919d7994815a68a36a1fc7
Bartuz <filip@bartuzi.me>
2013-11-04 21:15:57 +0100

    5b done, results in zadane5b.txt

--- 29.10.2013/zadanie5b.cpp
+++ 29.10.2013/zadanie5b.cpp
@@ -3,16 +3,53 @@
 #include <string>
 using namespace std;
 int main(int argc, char *argv[]) {
-	int temp,max,i,k;
+	int max, i, tab[10000], temp, sumaCiagu, sumaMniejszegoCiagu;
 	ifstream file;
 	string nazwaPliku;
-	for (int j = 1; j < 4; j++) { //petla ktora słuzy do ładowanie kolejnych plików.
-		nazwaPliku=""; // nazwa pliku zmienia się co nowy krok w pętli
-		nazwaPliku += "dane5-" + to_string(j) + ".txt"; // ustawiam zmienną nazwePliku na nową wartość
-		file.open(nazwaPliku); // otwieram nowy plik
-		file >> temp;
-		k = 1;
-		
-		file.close(); // i ów plik zamykam
+	ofstream fout("zadanie5b.txt");
+//	cout << "przed petla" <<endl;
+	for (int j = 1; j < 4; j++) {
+		nazwaPliku="";
+		nazwaPliku += "dane5-" + to_string(j) + ".txt";
+		file.open(nazwaPliku);
+		i = 0;
+		while (file >> tab[i]) {
+			if (i == 0) {
+				max = tab[0];
+			}
+			if (max < tab[i]) max = tab[i];
+//			cout << tab[i] << " ";
+			i++;
+		}
+		sumaCiagu = 0;
+		for (int j = 0; j < i; j++) {
+			sumaCiagu += tab[j];
+		}
+		fout << endl << "sumaCiagu = " << sumaCiagu <<endl;
+		if (sumaCiagu > max) {
+			max = sumaCiagu;
+		}
+		sumaMniejszegoCiagu = sumaCiagu;
+		for (int m = -1; m < i; m++) {
+//			cout <<  m  << " - ";
+			if (m >= 0) {
+				sumaMniejszegoCiagu -= tab[m];
+			} 
+			temp = sumaMniejszegoCiagu;
+//							cout << "dla "<< sumaMniejszegoCiagu+tab[m]  << "-" << tab[m] << "=" << temp <<  endl;
+			if (temp > max) { 
+				max = temp;
+			}	
+			for (int n = i-1; n > m; n-- ) {
+//				cout << n << endl;;
+				temp -= tab[n];
+//				cout << "\t" << temp+tab[n] << "-" << tab[n] << "=" << temp << endl;
+				if (temp > max) {
+					max = temp;
+				}	
+			}
+		}
+		fout << "dla " << nazwaPliku << " maksymalna suma to "  << max << endl;
+		file.close();	
 	}
 }

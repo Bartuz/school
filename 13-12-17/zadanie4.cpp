aaed7eafdf49745019a676eb662c71028938c46f
Bartuz <filip@bartuzi.me>
2013-12-18 03:48:23 +0100

    updates

--- 13-12-17/zadanie4.cpp
+++ 13-12-17/zadanie4.cpp
@@ -9,27 +9,29 @@ int main(int argc, char *argv[]) {
 	ofstream wout("odp_4b.txt");
 	ifstream fin("anagram.txt");
 	string temp,pattern,tab[5];
-	int isAnagram = 0, counter = 0;
+	int isAnagram = 0, counter = 0, isSameLength;
 	for (int k = 0; k<200;k++) {
 		for (int i = 0; i<5;i++) {
 			fin >> tab[i];
+//			cout << tab[i] << " ";
 			if (!counter){ 
-				counter= tab[0].length();
-				pattern = tab[0];
+				counter= tab[i].length();
+				pattern = tab[i];
 				isAnagram = 1;
+				isSameLength = 1;
 				sort(pattern.begin(),pattern.end());
 				continue;
 			}
 			
 			if (counter!=tab[i].length()) {
-				break;
+				isSameLength = 0;
 			}
 			temp = tab[i];
 			sort(temp.begin(),temp.end());
 			if (isAnagram && temp  != pattern) {
 				isAnagram = 0;
 			}
-			if (i == 4){
+			if (i == 4 && isSameLength){
 				for (int j = 0; j < 5; j++) {
 					fout << tab[j] << " ";
 					if (isAnagram) wout << tab[j] << " ";
@@ -40,4 +42,5 @@ int main(int argc, char *argv[]) {
 		}
 		counter = 0;
 	}
+	cout << tab[0] << " " << tab[1] << " " << tab[2] << " " << tab[3] << " " << tab[4]  ;
 }
